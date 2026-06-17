7c2ca05009312234856aaf4401866aecb0065175
gustavoprs <gustavopolicarporsch@gmail.com>
2026-06-17 16:47:22 -0300

    feat: add scroll area to project panel

--- src/components/my-zed/workspace/project-panel/index.tsx
+++ src/components/my-zed/workspace/project-panel/index.tsx
@@ -1,9 +1,12 @@
 import FileTree from "#/components/my-zed/workspace/project-panel/file-tree"
+import { ScrollArea } from "#/components/ui/scroll-area"
 
 export default function ProjectPanel() {
 	return (
 		<section className="flex flex-col h-full bg-mz-panel-background">
-			<FileTree />
+			<ScrollArea className={"w-full h-full"}>
+				<FileTree />
+			</ScrollArea>
 		</section>
 	)
 }
